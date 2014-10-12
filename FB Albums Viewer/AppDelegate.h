0e59800fa5e77f9dd988330e68ac06b1d412a4e1
Ryan Luce <ryan@ryanluce.com>
2014-10-11 17:16:13 -0700

    Initial Commit

---
+++ FB Albums Viewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FB Albums Viewer
+//
+//  Created by Ryan Luce on 10/11/14.
+//  Copyright (c) 2014 Ryan Luce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
